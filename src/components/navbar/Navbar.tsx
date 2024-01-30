036cd945e289fee2751b0b812e88c334765159ba
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-01-30 11:15:16 +0600

    update theme

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar: FC = () => {
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>lamablog</div>
+      <div className={styles.logo}>blogsite</div>
       <div className={styles.links}>
         <ThemeToggle />
         <Link href="/" className={styles.link}>
